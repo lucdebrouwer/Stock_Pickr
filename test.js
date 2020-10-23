4572f282baafe2fcb9fba2ae6b60cc6654ca4880
Luc <34102101+lucdebrouwer@users.noreply.github.com>
2020-10-23 13:26:40 +0200

    Update test.js
    Memelord

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
 const meow = () => {
   console.log("Asf ");
+  console.log("Daniel is a pioneer in making memes great again. With his appearance on the meme board he, and only he himself can make stonks rise!")
+  console.log("Who let the Dan out, who? who? who?");
 };
